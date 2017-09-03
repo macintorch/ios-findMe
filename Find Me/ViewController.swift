6de680b7ef57fa898a9c1ecab7a2148e7fa0c578
Ainor Syahrizal <macintorch@gmail.com>
2017-09-04 05:06:28 +0800

    get user current location

--- Find Me/ViewController.swift
+++ Find Me/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 import CoreLocation
+import MapKit
 
-class ViewController: UIViewController,CLLocationManagerDelegate {
+class ViewController: UIViewController,CLLocationManagerDelegate, MKMapViewDelegate {
+    @IBOutlet weak var map: MKMapView!
     
     var locationManager = CLLocationManager()
 
@@ -33,7 +35,25 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        print(locations)
+        let userLocation: CLLocation = locations[0]
+        
+        let latitude = userLocation.coordinate.latitude
+        
+        let longitude = userLocation.coordinate.longitude
+        
+        let latDelta: CLLocationDegrees = 0.05
+        
+        let lonDelta: CLLocationDegrees = 0.05
+        
+        let span = MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta)
+        
+        let location = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        self.map.setRegion(region, animated: true)
+        
+        
         
     }
 
